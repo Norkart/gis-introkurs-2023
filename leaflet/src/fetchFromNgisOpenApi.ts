9bbd7bb38b64572665e86e1b45a22048ffb8282d
Atle Frenvik Sveen <atle.sveen@gmail.com>
2023-01-17 10:21:01 +0100

    add filter

--- leaflet/src/fetchFromNgisOpenApi.ts
+++ leaflet/src/fetchFromNgisOpenApi.ts
@@ -4,11 +4,9 @@ const user = process.env.NGIS_OPEN_API_USER;
 const pass = process.env.NGIS_OPEN_API_PASS;
 
 export const fetchFromNgisOpenApi = (bounds: string) => {
-  const url = `https://openapi-test.kartverket.no/v1/datasets/63cb2b40-1461-4a9a-90c1-446ef0ee42f4/features?bbox=${bounds}&crs_EPSG=4326&normalized_for_visualization=true`;
-  fetchWithBasicAuth(url, user, pass, {
+  const url = `https://openapi-test.kartverket.no/v1/datasets/63cb2b40-1461-4a9a-90c1-446ef0ee42f4/features?bbox=${bounds}&crs_EPSG=4326&normalized_for_visualization=true&query=eq(*,Bygning)`;
+  return fetchWithBasicAuth(url, user, pass, {
     "X-Client-Product-Version": "KartAITest",
     Accept: "application/vnd.kartverket.sosi+json; version=1.0",
-  })
-    .then((r) => r.json())
-    .then((d) => console.log(d));
+  }).then((r) => r.json());
 };
